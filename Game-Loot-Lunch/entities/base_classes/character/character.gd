feb40cef808478032604dfd0c2db0f9ecb687ee9
limamon <ramon.uerjdesc@gmail.com>
2026-05-02 22:29:30 -0300

    feat player e inimigos: continuação da implementação do sistema de attack

--- Game-Loot-Lunch/entities/base_classes/character/character.gd
+++ Game-Loot-Lunch/entities/base_classes/character/character.gd
@@ -3,6 +3,7 @@ class_name Character
 
 const FRICTION: float = 0.15
 
+@export var hp: int = 2
 @export var accerelation: int = 40
 @export var max_speed: int = 100
 
@@ -20,3 +21,13 @@ func move() -> void:
 	mov_direction = mov_direction.normalized()
 	velocity += mov_direction * accerelation
 	velocity = velocity.limit_length(max_speed)
+	
+
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	if hp > 0:
+		state_machine.set_state(state_machine.states.hurt)
+		velocity += dir * force
+	else:
+		state_machine.set_state(state_machine.states.dead)
+		velocity += dir * force * 2
--- Game-Loot-Lunch/entities/player/fsm/player_fsm.gd
+++ Game-Loot-Lunch/entities/player/fsm/player_fsm.gd
@@ -3,6 +3,9 @@ extends FiniteStateMachine
 func _init() -> void:
 	_add_state("idle")
 	_add_state("move")
+	_add_state("hurt")
+	_add_state("dead")
+
 	
 func _ready() -> void:
 	set_state(states.idle)
@@ -22,8 +25,10 @@ func _get_transition() -> int:
 		states.move:
 			if parent.velocity.length() < 10:
 				return states.idle
+		states.hurt:
+			if not animation_player.is_playing():
+				return states.idle
 	return -1
-	
 	
 func _enter_state(_previous_state: int, new_state: int) -> void:
 	match new_state:
@@ -31,3 +36,7 @@ func _enter_state(_previous_state: int, new_state: int) -> void:
 			animation_player.play("idle")
 		states.move:
 			animation_player.play("move")
+		states.hurt:
+			animation_player.play("hurt")
+		states.dead:
+			animation_player.play("dead")
