46bc0fb4f102f09ad76d72b7327eb2a06631a891
Clique33 <gabriegcc@gmail.com>
2026-05-12 23:39:50 -0300

    feat(inventorycell)/fix(item): added working inventory_cell and its test. changed items to be Sprite2D.

--- Game-Loot-Lunch/entities/base_classes/item/item.gd
+++ Game-Loot-Lunch/entities/base_classes/item/item.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Sprite2D
 class_name Item
 
 @export var item_name : String		## Name of the Item
@@ -6,13 +6,6 @@ class_name Item
 @export var is_ingredient : bool	## Ingredients can be used in recipees
 
 
-var sprite : Sprite2D
-
-
 func _ready() -> void:
-	for node in get_children():
-		if node is Sprite2D:
-			sprite = node
-			break
-	if not sprite:
-		push_error("Item ",item_name," must have a Sprite2D as child")
+	if not texture:
+		push_error("Item ",item_name," must have a Texture")
