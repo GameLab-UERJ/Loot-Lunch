6351cfebb9ed2cf6a449502718f1556b3865b304
JPLMB <145187137+JPLMB@users.noreply.github.com>
2026-05-21 19:28:08 -0300

    feat(TimeCycle): Add day/night cycle timer and signals
    Introduce cycle_duration (exported, default 10s) and an internal _timer to automatically toggle TimeState between DAY and NIGHT in _process. Add day_started and night_started signals and emit them from change_state so other nodes can react to transitions; remove the previous debug print.

--- Game-Loot-Lunch/systems/TimeCycle/time_cycle.gd
+++ Game-Loot-Lunch/systems/TimeCycle/time_cycle.gd
@@ -1,13 +1,38 @@
 extends Node
 
 
+signal day_started
+signal night_started
+
+
 enum TimeState {
 	DAY,
 	NIGHT,
 }
 
 
+@export var cycle_duration: float = 10.0
+
+
 var current_state: TimeState = TimeState.DAY
+
+var _timer: float = 0.0
+
+
+func _process(delta: float) -> void:
+	_timer += delta
+
+	if _timer < cycle_duration:
+		return
+
+	_timer = 0.0
+
+	match current_state:
+		TimeState.DAY:
+			change_state(TimeState.NIGHT)
+
+		TimeState.NIGHT:
+			change_state(TimeState.DAY)
 
 
 func change_state(new_state: TimeState) -> void:
@@ -16,4 +41,9 @@ func change_state(new_state: TimeState) -> void:
 
 	current_state = new_state
 
-	print("Time state changed to: %s" % TimeState.keys()[current_state])
+	match current_state:
+		TimeState.DAY:
+			day_started.emit()
+
+		TimeState.NIGHT:
+			night_started.emit()
