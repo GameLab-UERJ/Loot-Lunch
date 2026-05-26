b77af6646cdf96daf891a7678e19bc52bb93ef75
Clique33 <gabriegcc@gmail.com>
2026-05-26 17:14:23 -0300

    feat(music): added music to all levels. added sound of bells when entering shop.

--- Game-Loot-Lunch/tests/levels/test_levels.gd
+++ Game-Loot-Lunch/tests/levels/test_levels.gd
@@ -7,6 +7,8 @@ var combat_zone : Node2D
 @onready var inside_house: Node2D = get_node("InsideHouse") if self.has_node("InsideHouse") else null
 @onready var external_house: Node2D = get_node("ExternalHouse") if self.has_node("ExternalHouse") else null
 @onready var shop_room: Node2D = get_node("ShopRoom") if self.has_node("ShopRoom") else null
+@onready var music_stream_player: AudioStreamPlayer = get_node("MusicStreamPlayer") if has_node("MusicStreamPlayer") else null
+@onready var audio_stream_player: AudioStreamPlayer = get_node("AudioStreamPlayer") if has_node("AudioStreamPlayer") else null
 
 
 
@@ -16,7 +18,7 @@ func _ready() -> void:
 		combat_zone = load("uid://d0pechf47plm7").instantiate()
 	if not inside_house:
 		inside_house = load("uid://beai1me2j13ef").instantiate()
-	if not external_house:
+	if not external_house: 
 		external_house = load("uid://d3ypiu36avnv1").instantiate()
 	if not shop_room:
 		shop_room = load("uid://dccphg7sgqmy7").instantiate()
@@ -24,22 +26,31 @@ func _ready() -> void:
 
 
 func _on_player_can_leave_house() -> void:
+	_on_change_scene()
 	external_house.get_node("ExternalHouse").player_start_position = external_house.get_node("HouseEntrance")
 	EasyTransition.transition_to_node(external_house,1.5,EasyTransition.TransitionAnim.FADE)
 
 
 func _on_player_can_enter_house() -> void:
+	_on_change_scene()
 	EasyTransition.transition_to_node(inside_house,1.5,EasyTransition.TransitionAnim.FADE)
 
 
 func _on_player_can_leave_shop() -> void:
+	_on_change_scene()
 	external_house.get_node("ExternalHouse").player_start_position = external_house.get_node("ShopEntrance")
 	EasyTransition.transition_to_node(external_house,1.5,EasyTransition.TransitionAnim.FADE)
 
 
 func _on_player_can_enter_shop() -> void:
+	_on_change_scene()
 	EasyTransition.transition_to_node(shop_room,1.5,EasyTransition.TransitionAnim.FADE)
 
 
 func _on_player_can_enter_cambat_zone() -> void:
+	_on_change_scene()
 	EasyTransition.transition_to_node(combat_zone,1.5,EasyTransition.TransitionAnim.FADE)
+
+func _on_change_scene(duration : float = 1.5) -> void:
+	if music_stream_player:
+		create_tween().tween_property(music_stream_player,"volume_db",-30,duration)
