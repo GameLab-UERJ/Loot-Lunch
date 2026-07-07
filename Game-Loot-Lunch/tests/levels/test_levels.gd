934c43a91fbf5daf820e93d80b98501b866a3d68
Clique33 <gabriegcc@gmail.com>
2026-07-07 11:33:53 -0300

    chore(names): fixed naming problems. fix(test_levels): there were two funcions with the same name. changed camera limits to make more sense.

--- Game-Loot-Lunch/tests/levels/test_levels.gd
+++ Game-Loot-Lunch/tests/levels/test_levels.gd
@@ -63,31 +63,32 @@ func _on_player_can_enter_internal_dungeon() -> void:
 	_on_change_scene()
 	EasyTransition.transition_to_node(internal_dungeon,1.5,EasyTransition.TransitionAnim.FADE)
 
+
 func _on_player_can_enter_dungeon_exterior() -> void:
 	_on_change_scene()
 	EasyTransition.transition_to_node(dungeon_exterior, 1.5, EasyTransition.TransitionAnim.FADE)
 
-func _on_player_can_enter_internal_dungeon() -> void:
-	_on_change_scene()
-	EasyTransition.transition_to_node(internal_dungeon, 1.5, EasyTransition.TransitionAnim.FADE)
 
 func _on_player_can_leave_internal_dungeon() -> void:
 	_on_change_scene()
 	EasyTransition.transition_to_node(dungeon_exterior, 1.5, EasyTransition.TransitionAnim.FADE)
 
+
 func _on_player_can_enter_dungeon_way() -> void:
 	_on_change_scene()
 	EasyTransition.transition_to_node(dungeon_way, 1.5, EasyTransition.TransitionAnim.FADE)
 
+
 func _on_player_can_leave_dungeon_way() -> void:
 	_on_change_scene()
 	EasyTransition.transition_to_node(dungeon_exterior, 1.5, EasyTransition.TransitionAnim.FADE)
-	
+
 
 func pause_game_menu() -> void:
 	if !get_parent().has_node("PauseMenu"):
 		pause_menu = pause_menu_scene.instantiate() as PauseMenu
 		call_deferred("add_sibling", pause_menu)
+
 
 func _on_change_scene(duration : float = 1.5) -> void:
 	if music_stream_player:
