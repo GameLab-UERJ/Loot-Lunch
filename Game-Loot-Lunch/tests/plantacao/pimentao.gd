cc3ca5480a23e4407dc7598890a5b38fb952a7d8
Rebeca Dias <dpfrebeca@gmail.com>
2026-07-10 17:13:02 -0300

    feat(plantacao): foi adicionado pequenas cenas de uma plantacao apenas para enfeite no momento.

---
+++ Game-Loot-Lunch/tests/plantacao/pimentao.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var item_name: String = "Pimentao"
+@export var raio_coleta: float = 20.0
+
+func _process(delta: float) -> void:
+	var player = get_tree().current_scene.find_child("Player", true, false)
+	
+	if player:
+		var distancia = global_position.distance_to(player.global_position)
+		if distancia < raio_coleta:
+			queue_free()
+			print("Essa desgraça funcionouuuu ( diferente do area_entered, por isso tive q por assim )")
