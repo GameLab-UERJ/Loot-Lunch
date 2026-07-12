9ff21c67e22f7f7a009834d063313c333621e8b9
Igor Amaral <igorbstm321@gmail.com>
2026-07-12 15:44:27 -0300

    feat(teste): 6 carnes stackaveis + 1 tomate, auto-add via script

--- Game-Loot-Lunch/tests/inventory/test_inventory.gd
+++ Game-Loot-Lunch/tests/inventory/test_inventory.gd
@@ -3,8 +3,12 @@ extends Node2D
 
 @onready var inventory: Inventory = $CanvasLayer/Inventory
 @onready var carne: Item = $Carne
+@onready var carne2: Item = $Carne2
+@onready var carne3: Item = $Carne3
+@onready var carne4: Item = $Carne4
+@onready var carne5: Item = $Carne5
+@onready var carne6: Item = $Carne6
 @onready var tomate: Item = $Tomate
-@onready var farinha: Item = $Farinha 
 @onready var selected_cell: Label = $VBoxContainer/SelectedCell
 @onready var selected_item: Label = $VBoxContainer/SelectedItem
 @onready var selected_pos: Label = $VBoxContainer/SelectedPos
@@ -14,23 +18,21 @@ extends Node2D
 func _ready() -> void:
 	tutorial.text = '''
 	  Press    I    to    show/hide    Inventory
-	  Left    click    to    select/move    items
-	  Right    click    to    cancel    selection/remove    items'''
-					
-	carne.connect("picked_up",inventory.add_item)
-	tomate.connect("picked_up",inventory.add_item)
-	farinha.connect("picked_up",inventory.add_item)
-	'''inventory.add_item(carne)
+	  Left    click    (stack)    to    pick    1  unit
+	  Shift+click    (stack)    to    pick    all
+	  Right    click    (stack)    to    split
+	  Right    click    (bg)    to    drop    item'''
+
+	# Adiciona 6 Carnes e 1 Tomate automaticamente
+	# As primeiras 5 carnes empilham na mesma célula (stack_size=5)
+	# A 6ª vai pra outra célula; o Tomate vai pra uma terceira.
+	inventory.add_item(carne)
+	inventory.add_item(carne2)
+	inventory.add_item(carne3)
+	inventory.add_item(carne4)
+	inventory.add_item(carne5)
+	inventory.add_item(carne6)
 	inventory.add_item(tomate)
-	inventory.add_item(farinha)
-	print("root:\n",get_children(),"\n-----------")
-	print("grid:")
-	inventory.print_inventory_cells()
-	print("\n-----------")
-	await get_tree().create_timer(5).timeout
-	inventory.remove_item_at(Vector2i.RIGHT).force_follow_mouse()
-	print(get_children())
-	inventory.print_inventory_cells()'''
 
 
 func _physics_process(_delta: float) -> void:
