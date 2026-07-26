756d4b01f60a7b43d158e9b65344e876dfb309af
Clique33 <gabriegcc@gmail.com>
2026-07-26 18:46:33 -0300

    asset(oven): added placeholder oven. feat(cooking_workbench): added oven.

--- Game-Loot-Lunch/entities/base_classes/workbench.gd
+++ Game-Loot-Lunch/entities/base_classes/workbench.gd
@@ -6,14 +6,15 @@ enum Type {None, Common, Cutter, Drying, Cooker}
 
 
 @export var type : Workbench.Type
+@export var outline_width : int = 1
 
 
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: CanvasItem = $Sprite2D
 
 
 func _on_interactable_area_interact_with_player() -> void:
 	GlobalData.current_workbench = type
-	sprite.material.set("shader_parameter/width",1)
+	sprite.material.set("shader_parameter/width",outline_width)
 
 
 func _on_interactable_area_stop_interact_with_player() -> void:
