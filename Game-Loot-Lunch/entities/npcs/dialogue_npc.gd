685da6e397ef2812a3bcb777293cd3dd52ca2b0e
Clique33 <gabriegcc@gmail.com>
2026-08-06 17:10:59 -0300

    feat(npcs): added outline when plyer can interact

--- Game-Loot-Lunch/entities/npcs/dialogue_npc.gd
+++ Game-Loot-Lunch/entities/npcs/dialogue_npc.gd
@@ -13,11 +13,27 @@ var dialogue_balloon: Node
 
 
 @onready var interaction_label: Label = %InteractionLabel
+@onready var animated_sprite: AnimatedSprite2D = get_node_or_null("AnimatedSprite2D"):
+	set(value):
+		animated_sprite = value
+		if animated_sprite:
+			animated_sprite.material = load("res://resources/shaders/outline_material.tres").duplicate_deep()
+			animated_sprite.material.set("shader_parameter/outline_color",Color.WHITE)
+			animated_sprite.material.set("shader_parameter/width",0)
+@onready var sprite: Sprite2D = get_node_or_null("Sprite2D"):
+	set(value):
+		sprite = value
+		if sprite:
+			sprite.material = load("res://resources/shaders/outline_material.tres").duplicate_deep()
+			sprite.material.set("shader_parameter/outline_color",Color.WHITE)
+			sprite.material.set("shader_parameter/width",0)
 
 
 func _ready() -> void:
 	$Area2D.player_entered.connect(_on_player_entered)
 	$Area2D.player_exited.connect(_on_player_exited)
+	animated_sprite = animated_sprite
+	sprite = sprite
 
 
 func _process(_delta: float) -> void:
@@ -32,10 +48,18 @@ func _process(_delta: float) -> void:
 
 
 func _on_player_entered(_player) -> void:
+	if animated_sprite:
+		animated_sprite.material.set("shader_parameter/width",1)
+	if sprite:
+		sprite.material.set("shader_parameter/width",1)
 	interaction_label.show()
 	player_in_range = true
 
 
 func _on_player_exited(_player) -> void:
+	if animated_sprite:
+		animated_sprite.material.set("shader_parameter/width",0)
+	if sprite:
+		sprite.material.set("shader_parameter/width",0)
 	interaction_label.hide()
 	player_in_range = false
--- Game-Loot-Lunch/entities/npcs/guide_man/guide_man.gd
+++ Game-Loot-Lunch/entities/npcs/guide_man/guide_man.gd
@@ -1,26 +1,43 @@
 extends CharacterBody2D
 
+
+const DIALOGUE_RESOURCE: DialogueResource = preload("res://entities/npcs/dialogos/guide_man_quest.dialogue")
+
+
+var can_interact: bool = false
+
+
 @onready var interactable_area: GenericInteractableArea = $InteractableArea
 @onready var interaction_label: Label = %InteractionLabel
 @onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = get_node_or_null("AnimatedSprite2D"):
+	set(value):
+		animated_sprite = value
+		if animated_sprite:
+			animated_sprite.material = load("res://resources/shaders/outline_material.tres").duplicate_deep()
+			animated_sprite.material.set("shader_parameter/outline_color",Color.WHITE)
+			animated_sprite.material.set("shader_parameter/width",0)
 
-var can_interact: bool = false
-const DIALOGUE_RESOURCE: DialogueResource = preload("res://entities/npcs/dialogos/guide_man_quest.dialogue")
 
 func _ready() -> void:
 	interaction_label.hide()
 	interactable_area.player_entered.connect(_on_player_entered)
 	interactable_area.player_exited.connect(_on_player_exited)
+	animated_sprite = animated_sprite
+
 
 func _on_player_entered(_player: Player) -> void:
+	animated_sprite.material.set("shader_parameter/width",1)
 	can_interact = true
 	interaction_label.show()
 	anim_sprite.play("wave")
 
+
 func _on_player_exited(_player: Player) -> void:
+	animated_sprite.material.set("shader_parameter/width",0)
 	can_interact = false
 	interaction_label.hide()
-	#anim_sprite.play("idle")
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if can_interact and event.is_action_pressed("interact"):
